eb017babd1cc03c7735b79836205a50880497377
il-yoo <dbdlf05049@naver.com>
2026-06-21 16:24:19 +0900

    Update fund_radar.py

--- fund_radar.py
+++ fund_radar.py
@@ -1,27 +1,72 @@
 import requests
 import os
+from datetime import datetime, timedelta
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
+DART_API_KEY = os.environ["DART_API_KEY"]
 
-message = """
-🔔 Fund Radar 테스트 성공!
+today = datetime.today()
+yesterday = today - timedelta(days=1)
 
-✅ GitHub Actions 정상
-✅ Telegram 연동 성공
-✅ DART API Secret 등록 완료
+bgn = yesterday.strftime("%Y%m%d")
+end = today.strftime("%Y%m%d")
 
-다음 단계부터 신규 펀드 탐색을 시작합니다.
-"""
+url = "https://opendart.fss.or.kr/api/list.json"
 
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+params = {
+    "crtfc_key": DART_API_KEY,
+    "bgn_de": bgn,
+    "end_de": end,
+    "page_count": 100
+}
+
+r = requests.get(url, params=params)
+data = r.json()
+
+msg = "🔔 Fund Radar\n\n"
+
+if data["status"] == "000":
+
+    keywords = [
+        "집합투자",
+        "ETF",
+        "증권신고서",
+        "효력발생",
+        "투자설명서"
+    ]
+
+    found = False
+
+    for item in data["list"]:
+
+        title = item["report_nm"]
+
+        if any(k in title for k in keywords):
+
+            found = True
+
+            msg += (
+                f"운용사 : {item['corp_name']}\n"
+                f"공시 : {title}\n"
+                f"날짜 : {item['rcept_dt']}\n\n"
+            )
+
+    if not found:
+        msg += "신규 펀드 관련 공시가 없습니다."
+
+else:
+
+    msg += "DART 조회 실패"
+
+telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
 requests.post(
-    url,
+    telegram_url,
     data={
         "chat_id": CHAT_ID,
-        "text": message
+        "text": msg
     }
 )
 
-print("Telegram message sent.")
+print(msg)
